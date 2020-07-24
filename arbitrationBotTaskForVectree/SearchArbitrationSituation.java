4482c063f76a9795f8488ec2e7ccd4f5b21bfcf1
ahilessprime <ahilessprime@gmail.com>
2020-07-24 23:48:20 +0300

    Issues #9

--- arbitrationBotTaskForVectree/SearchArbitrationSituation.java
+++ arbitrationBotTaskForVectree/SearchArbitrationSituation.java
@@ -1,5 +1,6 @@
 package arbitrationBotTaskForVectree;
 
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class SearchArbitrationSituation {
@@ -12,10 +13,22 @@ public class SearchArbitrationSituation {
     }
 
     public void startParsing(){
+        //здесь определяем ексекутор сервис, фор для массива и внутри него канкарент,
+        //что бы каждый парсинг шел на новом потоке
 
     }
 
     public static void setOrderBooksJava(ConcurrentHashMap<String, OrderBookJava> orderBooksJava) {
         SearchArbitrationSituation.orderBooksJava = orderBooksJava;
+    }
+
+    //метод который парсит данную ситуацию
+    private List parsingThisSiuation(OrderBookJava orderBookJava) {
+
+    return null; }
+
+    //метод который из пары валют делит на валюты, для их последующих сравнений
+    private String[] returnOfCurrencyFromAPair(String pairCurrency) {
+        return null;
     }
 }
